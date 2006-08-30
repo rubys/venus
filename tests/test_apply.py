f338e293ae5a87c54cf625f0a58c5acc86c55824
Sam Ruby <rubys@rubypad>
2006-08-30 12:43:34 -0400

    Test case for fancy theme

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -5,11 +5,15 @@ from planet import config, splice
 from xml.dom import minidom
 
 workdir = 'tests/work/apply'
-configfile = 'tests/data/apply/config.ini'
+configfile = 'tests/data/apply/config-%s.ini'
 testfeed = 'tests/data/apply/feed.xml'
 
 class ApplyTest(unittest.TestCase):
     def setUp(self):
+        testfile = open(testfeed)
+        self.feeddata = testfile.read()
+        testfile.close()
+
         try:
              os.makedirs(workdir)
         except:
@@ -20,13 +24,9 @@ class ApplyTest(unittest.TestCase):
         shutil.rmtree(workdir)
         os.removedirs(os.path.split(workdir)[0])
 
-    def test_apply(self):
-        testfile = open(testfeed)
-        feeddata = testfile.read()
-        testfile.close()
-
-        config.load(configfile)
-        splice.apply(feeddata)
+    def test_apply_asf(self):
+        config.load(configfile % 'asf')
+        splice.apply(self.feeddata)
 
         # verify that selected files are there
         for file in ['index.html', 'default.css', 'images/foaf.png']:
@@ -46,3 +46,19 @@ class ApplyTest(unittest.TestCase):
         html.close()
         self.assertEqual(3, lang)
         self.assertEqual(12, content)
+
+    def test_apply_fancy(self):
+        config.load(configfile % 'fancy')
+        splice.apply(self.feeddata)
+
+        # verify that selected files are there
+        for file in ['index.html', 'planet.css', 'images/jdub.png']:
+            path = os.path.join(workdir, file)
+            self.assertTrue(os.path.exists(path), path)
+            self.assertTrue(os.stat(path).st_size > 0)
+
+        # verify that index.html is well formed, has content, and xml:lang
+        html = open(os.path.join(workdir, 'index.html')).read()
+        self.assertTrue('<h1>test planet</h1>' in html)
+        self.assertTrue('<h4><a href="http://example.com/2">Venus</a></h4>'
+            in html)
