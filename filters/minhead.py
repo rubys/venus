b25df8b9d4da2352dd7d04e7e8dcffc99a9fa49d
Sam Ruby <rubys@intertwingly.net>
2007-06-15 10:42:01 -0400

    Add minhead filter, and resync opml-top100.ini

---
+++ filters/minhead.py
@@ -0,0 +1,36 @@
+#
+# Ensure that all headings are below a permissible maximum (like h3).
+# If not, all heading levels will be changed to conform.
+# Note: this may create "illegal" heading levels, like h7 and beyond.
+#
+
+import sys
+from xml.dom import minidom, XHTML_NAMESPACE
+
+# determine permissible minimimum heading
+if '--min' in sys.argv:
+  minhead = int(sys.argv[sys.argv.index('--min')+1])
+else:
+  minhead=3
+
+# parse input stream
+doc = minidom.parse(sys.stdin)
+
+# search for headings below the permissable minimum
+first=minhead
+for i in range(1,minhead):
+  if doc.getElementsByTagName('h%d' % i):
+    first=i
+    break
+
+# if found, bump all headings so that the top is the permissible minimum
+if first < minhead:
+  for i in range(6,0,-1):
+    for oldhead in doc.getElementsByTagName('h%d' % i):
+      newhead = doc.createElementNS(XHTML_NAMESPACE, 'h%d' % (i+minhead-first))
+      for child in oldhead.childNodes:
+        newhead.appendChild(child)
+      oldhead.parentNode.replaceChild(newhead, oldhead)
+
+# return (possibly modified) document
+print doc.toxml('utf-8')
