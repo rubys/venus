23cd6be041689f3e015b29723c8415263060ec7d
Sam Ruby <rubys@intertwingly.net>
2007-07-05 15:01:19 -0400

    http://amit.chakradeo.net/2007/07/05/django-unicode-integration-fix-for-venus-djando-template/

--- planet/shell/dj.py
+++ planet/shell/dj.py
@@ -40,7 +40,7 @@ def run(script, doc, output_file=None, options={}):
         reluri = os.path.splitext(os.path.basename(output_file))[0]
         context['url'] = urlparse.urljoin(config.link(),reluri)
         f = open(output_file, 'w')
-        f.write(t.render(context))
+        f.write(t.render(context).encode('utf-8'))
         f.close()
     else:
         # @@this is useful for testing purposes, but does it 
