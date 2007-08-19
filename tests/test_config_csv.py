597f0ea2fe80fe4f8e0ea00316a2ccaf8f614866
Edward O'Connor <ted@oconnor.cx>
2007-08-18 20:12:07 -0700

    Made test_config_csv clean up after itself, so test_rlists would pass

--- tests/test_config_csv.py
+++ tests/test_config_csv.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-import unittest
+import os, shutil, unittest
 from planet import config
+
+workdir = os.path.join('tests', 'work', 'config', 'cache')
 
 class ConfigCsvTest(unittest.TestCase):
     def setUp(self):
         config.load('tests/data/config/rlist-csv.ini')
+
+    def tearDown(self):
+        shutil.rmtree(workdir)
+        os.removedirs(os.path.split(workdir)[0])
 
     # administrivia
 
