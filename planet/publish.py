d9cef10930e62c247d44a4f626e5f124575e2eaf
Matt Domsch <Matt_Domsch@dell.com>
2010-06-15 12:26:39 -0500

    only publish atom, rss10, and rss20 via PuSH

--- planet/publish.py
+++ planet/publish.py
@@ -7,7 +7,7 @@ def publish(config):
     link = config.link()
     if hub and link:
         for root, dirs, files in os.walk(config.output_dir()):
-            xmlfiles = [urlparse.urljoin(link, f) for f in files if f.endswith('.xml')]
+            xmlfiles = [urlparse.urljoin(link, f) for f in files if f in ['atom.xml', 'rss10.xml', 'rss20.xml']]
             try:
                 PuSH.publish(hub, xmlfiles)
             except PuSH.PublishError, e:
